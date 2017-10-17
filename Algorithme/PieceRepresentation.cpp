409e0bf8860b0e74f73e33bb35ca7ed8fc6928ea
zineb2307 <zineb.loukili1@gmail.com>
2017-10-17 22:07:04 +0200

    Update class PieceRepresentation: rotation

--- Algorithme/PieceRepresentation.cpp
+++ Algorithme/PieceRepresentation.cpp
@@ -1,8 +1,14 @@
 #include "PieceRepresentation.h"
 
 
+PieceRepresentation::PieceRepresentation()
+{
+
+}
+
+
 PieceRepresentation::PieceRepresentation(int x, int y, int z,int *vect):m_x(x),m_y(y),m_z(z){
-for(int i(0); i<vect.length; ++i)
+for(int i(0); i<sizeof(vect); ++i)
 
 {
 
@@ -18,7 +24,7 @@ PieceRepresentation::PieceRepresentation(const PieceRepresentation &p)
     m_y = p.m_y;
     m_z = p.m_z;
 
-for(int i(0); i<p.m_vecteur.length; ++i)
+for(int i(0); i<sizeof(p.m_vecteur); ++i)
 
 {
 
@@ -26,4 +32,119 @@ for(int i(0); i<p.m_vecteur.length; ++i)
 
 }
 }
+
+
+PieceRepresentation PieceRepresentation::rotateX()
+{
+
+
+
+
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<sizeof(m_vecteur);j++){
+	  
+	  
+   matrice_x[1][1]=1;
+   matrice_x[1][2]=0;
+   matrice_x[1][3]=0;
+   matrice_x[1][4]=0;
+   
+   matrice_x[2][1]=0;
+   matrice_x[2][2]=cos(teta);
+   matrice_x[2][3]=sin(teta);
+   matrice_x[2][4]=0;
+   
+   matrice_x[3][1]=0;
+   matrice_x[3][2]=-sin(teta);
+   matrice_x[3][3]=cos(teta);
+   matrice_x[3][4]=0;
+   
+   matrice_x[4][1]=0;
+   matrice_x[4][2]=0;
+   matrice_x[4][3]=0;
+   matrice_x[4][4]=1; 
+   
+   
+           m_vecteur[i] += matrice_x[i][j] * m_vecteur[j];
+        }
+    }
+    return *this;
+}
+
+
+PieceRepresentation PieceRepresentation::rotateY()
+{
+  
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<sizeof(m_vecteur);j++){
+	  
+	  
+   matrice_y[1][1]=cos(teta);
+   matrice_y[1][2]=0;
+   matrice_y[1][3]=-sin(teta);
+   matrice_y[1][4]=0;
+   
+   matrice_y[2][1]=0;
+   matrice_y[2][2]=1;
+   matrice_y[2][3]=0;
+   matrice_y[2][4]=0;
+   
+   matrice_y[3][1]=sin(teta);
+   matrice_y[3][2]=0;
+   matrice_y[3][3]=cos(teta);
+   matrice_y[3][4]=0;
+   
+   matrice_y[4][1]=0;
+   matrice_y[4][2]=0;
+   matrice_y[4][3]=0;
+   matrice_y[4][4]=1; 
+   
+   
+           m_vecteur[i] += matrice_y[i][j] * m_vecteur[j];
+        }
+    }
+    return *this;
+
+}
+
+PieceRepresentation PieceRepresentation::rotateZ()
+{
+
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<sizeof(m_vecteur);j++){
+	  
+	  
+   matrice_z[1][1]=cos(teta);
+   matrice_z[1][2]=sin(teta);
+   matrice_z[1][3]=0;
+   matrice_z[1][4]=0;
+   
+   matrice_z[2][1]=-sin(teta);
+   matrice_z[2][2]=cos(teta);
+   matrice_z[2][3]=0;
+   matrice_z[2][4]=0;
+   
+   matrice_z[3][1]=0;
+   matrice_z[3][2]=0;
+   matrice_z[3][3]=1;
+   matrice_z[3][4]=0;
+   
+   matrice_z[4][1]=0;
+   matrice_z[4][2]=0;
+   matrice_z[4][3]=0;
+   matrice_z[4][4]=1; 
+   
+   
+           m_vecteur[i] += matrice_z[i][j] * m_vecteur[j];
+        }
+    }
+    return *this;
+
+}
+
+
+
  
--- Algorithme/PieceRepresentation.h
+++ Algorithme/PieceRepresentation.h
@@ -1,20 +1,51 @@
 #ifndef PIECEREPRENSENTATION_H
 #define PIECEREPRENSENTATION_H
 
+#include<iostream>
+#include <string>
+#include<math.h>
+#include<algorithm>
+
 
 
 class PieceRepresentation {
 
+  
+private:
     int m_x; 
     int m_y;
     int m_z;
     int m_vecteur[];
     
+protected:    
+    int matrice_x[][4];//la matrice de rotation selon l'axe X.
+    int matrice_y[][4];//la matrice de rotation selon l'axe Y.
+    int matrice_z[][4];//la matrice de rotation selon l'axe Z.
+    int teta;//l'angle de rotation de la pièce.
+    
  
 public:
+    
+   PieceRepresentation();
    
     PieceRepresentation(int x, int y, int z, int *vect);
     PieceRepresentation(const PieceRepresentation &);
+   
+    PieceRepresentation(int *tab)
+        {
+            for(int i=0;i<sizeof(m_vecteur);i++)
+           m_vecteur[i] = tab[i];
+        }
+      
+        void affiche()
+        {
+	  for(int i=0;i<sizeof(m_vecteur);i++)
+           // std::cout<<"("<<m_vecteur[0]<<","<<m_vecteur[1]<<","<<m_vecteur[2]<<")"<<std::endl;
+	    std::cout<<m_vecteur[i]<<std::endl;
+        }
+        
+
+        
 
  
 
@@ -27,7 +58,7 @@ public:
     void setY(int y){m_y=y;}
     int getY(){return m_y;}
     
-     void setZ(int x){m_z=z;}
+     void setZ(int z){m_z=z;}
     int getZ(){return m_z;}
 
 
@@ -36,9 +67,15 @@ public:
   int getValue(int x,int y,int z);
   void setValue(int x, int y,int z,int val);
   // getPieceRepresentation();
+  
   PieceRepresentation rotateX();
   PieceRepresentation rotateY();
   PieceRepresentation rotateZ();
+  
+  
+
+
+
   
   
   
@@ -46,6 +83,7 @@ public:
   
   
   
+   
 };
 
 #endif
